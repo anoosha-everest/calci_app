7cada3c8ba94d0395620c3aa0da0dcc72b87098f
Anoosha <anoosha.sanugula@everest.engineering>
2024-02-20 23:26:41 +0530

    adding calculator file

---
+++ calculator.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Welcome to EVEREST calculator";
+    cout<<"enter 1 for addition"<<endl<<"enter 2 for subtraction";
+    int n;
+    cin>>n;
+    int a,b;
+    cin>>a>>b;
+}
